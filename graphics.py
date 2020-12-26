5a950394b2ba2b2aa4ff60a4cb7b9dfc02aff34f
Evan <evanlavender13@gmail.com>
2020-12-26 11:53:42 -0500

    Some stuff

--- graphics.py
+++ graphics.py
@@ -9,31 +9,31 @@ def clear_screen(screen):
     return np.zeros_like(screen)
 
 
-def bresenham(screen, x1, y1, x2, y2):
+def bresenham(screen, x0, y1, x1, y2):
     # https://inst.eecs.berkeley.edu/~cs150/fa10/Lab/CP3/LineDrawing.pdf
     # TODO: make this return array of points instead of drawing itself.. maybe
 
-    steep = np.abs(y2 - y1) > np.abs(x2 - x1)
+    steep = np.abs(y2 - y1) > np.abs(x1 - x0)
     if steep:
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
+        x0, y1 = y1, x0
+        x1, y2 = y2, x1
 
-    if x1 > x2:
-        x1, x2 = x2, x1
+    if x0 > x1:
+        x0, x1 = x1, x0
         y1, y2 = y2, y1
 
     d_err = np.abs(y2 - y1)
     y_step = -1 if y1 > y2 else 1
-    dx = x2 - x1
+    dx = x1 - x0
 
     err = dx >> 1
     y = y1
 
-    for x in range(x1, x2):
+    for x in range(x0, x1):
         if steep:
-            draw_point_xy(screen, y, x)
+            draw_point(screen, y, x)
         else:
-            draw_point_xy(screen, x, y)
+            draw_point(screen, x, y)
 
         err -= d_err
         if err < 0:
@@ -42,31 +42,29 @@ def bresenham(screen, x1, y1, x2, y2):
 
 
 def draw_line(screen, p0, p1):
-    x1, y1 = p0
-    x2, y2 = p1
+    x0, y1 = p0
+    x1, y2 = p1
 
-    bresenham(screen, x1, y1, x2, y2)
-
-
-def draw_points_p(screen, points, value=255):
-    for p in points:
-        draw_point_p(screen, p, value=value)
+    bresenham(screen, x0, y1, x1, y2)
 
 
-def draw_point_p(screen, p, value=255):
-    draw_point_xy(screen, p[0], p[1], value=value)
-
-
-def draw_points_xy(screen, points, lines=False, value=255):
+def draw_points(screen, points, lines=False, connect=False, value=255):
     size = len(points)
+
     for i, (x, y) in enumerate(points):
-        draw_point_xy(screen, x, y, value=value)
+        draw_point(screen, x, y, value=value)
+
         if lines and i < size - 1:
-            x2, y2 = points[i + 1]
-            draw_line(screen, (x, y), (x2, y2))
+            x1, y2 = points[i + 1]
+            draw_line(screen, (x, y), (x1, y2))
+
+    if connect and lines:
+        x0, y1 = points[-1, 0], points[-1, 1]
+        x1, y2 = points[0, 0], points[0, 1]
+        draw_line(screen, (x0, y1), (x1, y2))
 
 
-def draw_point_xy(screen, x, y, value=255):
+def draw_point(screen, x, y, value=255):
     row = screen.shape[0] - 1 - y
     col = x
 
@@ -75,10 +73,3 @@ def draw_point_xy(screen, x, y, value=255):
 
 if __name__ == "__main__":
     screen = create_screen((250, 250))
-
-    p0 = (0, 0)
-    p1 = (249, 249)
-
-    draw_line(screen, p0, p1)
-    draw_point_p(screen, p0)
-    draw_point_p(screen, p1)
